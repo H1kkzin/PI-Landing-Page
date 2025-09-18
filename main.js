20570ec659bcf819f18b56e75026f4e091af394d
H1kkzin <murilonunes162@gmail.com>
2025-09-18 20:36:45 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('main section');
+    const options = {
+        threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
